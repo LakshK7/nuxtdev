73917819593550335dc5562ddc2d836e0c523070
LakshK7 <laksh@Lakshs-MacBook-Air.local>
2023-06-19 23:25:10 -0400

    minor changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar :elevation="2" rounded>
+    <v-app-bar :elevation="2" rounded color="primary">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>My Portfolio</v-toolbar-title>
     </v-app-bar>
@@ -8,7 +8,7 @@
     <!-- <v-card> -->
     <v-main>
       <v-container>
-        <v-row>
+        <v-row align-content="center" justify="center" style="margin-top: 20%">
           <v-col v-for="n in 3" :key="n" cols="4"
             ><v-card height="200"><slot></slot></v-card></v-col
         ></v-row>
