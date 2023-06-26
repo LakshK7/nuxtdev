1f493bbe6ed98ad9dfc553e6fbb78d39f1007a1f
LakshK7 <laksh@Lakshs-MacBook-Air.local>
2023-06-26 17:13:28 -0400

    deletions

--- docs/public/_nuxt/config.bce3615c.js
+++
@@ -1,1 +0,0 @@
-import{E as p,z as o,F as i}from"./entry.e054c00c.js";const f={},t=p(f);function s(){const n=o();return n._appConfig||(n._appConfig=i(t)),n._appConfig}export{s as u};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,9 +19,6 @@ export default defineNuxtConfig({
         },
       ],
     },
-  },
-
-  router: {
-    base: "/nuxtdev/",
+    baseURL: "/nuxtdev/",
   },
 });
